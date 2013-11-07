6c3efeacef81ad0c85ab1f5b06be0e24ffbe95ca
PaulMag <paulms@ulrik.uio.no>
2013-11-07 18:02:07 +0100

    Included timeloop. Is close to working. Some indexing border condition error.

--- backwardEuler.cpp
+++ backwardEuler.cpp
@@ -5,22 +5,14 @@
 
 using namespace arma;
 
-vec backwardEuler() {
+vec backwardEuler(int n, double time, double dt) {
 
-    int n = 100;
-    double alpha = 0.123; // arbitrary value
+    double alpha = dt * (n+1) * (n+1); // alpha = dt / dx^2
 
     vec a(n);
     vec b(n);
     vec b_new(n);
     vec c(n);
-
-    vec u     = zeros<vec>(n); // previous timestep
-    vec u_new = zeros<vec>(n);
-    vec v     = zeros<vec>(n); // next timestep
-    u(0)     = 1;
-    u_new(0) = 1;
-    v(0)     = 1;
 
     // Fill in arrays:
     for (int i=0; i<n; i++) {
@@ -29,23 +21,41 @@ vec backwardEuler() {
         c(i) = -alpha;
     }
     a(0)   = 0; // parts that "stick outside" matrix
-    c(n-1) = 0;
+    c(n-1) = 0; // no point in this a.t.m.
 
-    // The algorithm:
-    b_new(0) = b(0);
-    u_new(0) = u(0);
+    // Initial value:
+    vec u = zeros<vec>(n); // previous timestep
+    u(0)  = 1;
+    vec u_new(n);
+    vec v(n); // next timestep
+    v(0)  = u(0);
 
-    for (int i=1; i<n; i++) {
-        double factor = a(i) / b_new(i-1); // avoids doing this twice
+    cout << u << endl; // for testing
 
-        b_new(i) = b(i) - c(i-1) * factor;
+    // Time loop:
+    for (double j=0; j*dt < time; j++) {
 
-        u_new(i) = u(i) - u_new(i-1) * factor;
-    }
+        // Algorithm:
+        b_new(0) = b(0);
+        u_new(0) = u(0);
 
-    v(n-1) = u(n-1) / b_new(n-1);
-    for (int i=n-2; i>0; i--) {
-        v(i) = (u_new(i) - v(i+1) * c(1)) / b_new(i); //unsure which c,no matter
+        for (int i=1; i<n; i++) {
+            double factor = a(i) / b_new(i-1); // avoids doing this twice
+
+            b_new(i) = b(i) - c(i-1) * factor;
+
+            u_new(i) = u(i) - u_new(i-1) * factor;
+        }
+
+        v(n-1) = u(n-1) / b_new(n-1);
+        for (int i=n-2; i>0; i--) {
+            v(i) = (u_new(i) - v(i+1) * c(i)) / b_new(i); //unsure which c,no matter
+        }
+
+        for (int i=0; i<n; i++) {
+            u(i) = v(i);
+        }
+        //cout << u << endl; // for testing
     }
 
     return v;
