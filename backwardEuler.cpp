f43c5a71401fef05e9c24c33dacd7fb6e32ce409
PaulMag <paulms@ulrik.uio.no>
2013-11-07 16:31:04 +0100

    Reverted to the same indexing used in project 1. It works, but may contain a small error.

--- backwardEuler.cpp
+++ backwardEuler.cpp
@@ -10,39 +10,42 @@ vec backwardEuler() {
     int n = 100;
     double alpha = 0.123; // arbitrary value
 
-    vec a(n-1);
-    vec b(n-1);     // previous timestep
-    vec b_new(n-1);
-    vec c(n-1);     // next timestep
+    vec a(n);
+    vec b(n);
+    vec b_new(n);
+    vec c(n);
 
-    vec u     = zeros<vec>(n+1);
-    vec u_new = zeros<vec>(n+1);
-    vec v     = zeros<vec>(n+1);
+    vec u     = zeros<vec>(n); // previous timestep
+    vec u_new = zeros<vec>(n);
+    vec v     = zeros<vec>(n); // next timestep
     u(0)     = 1;
     u_new(0) = 1;
     v(0)     = 1;
 
     // Fill in arrays:
-    for (int i=0; i<n-1; i++) {
+    for (int i=0; i<n; i++) {
         a(i) = -alpha;
         b(i) = 1 + 2 * alpha;
         c(i) = -alpha;
     }
     a(0)   = 0; // parts that "stick outside" matrix
-    c(n-2) = 0;
+    c(n-1) = 0;
 
     // The algorithm:
-    for (int i=1; i<n-1; i++) {
+    b_new(0) = b(0);
+    u_new(0) = u(0);
+
+    for (int i=1; i<n; i++) {
         double factor = a(i) / b_new(i-1); // avoids doing this twice
 
         b_new(i) = b(i) - c(i-1) * factor;
 
-        u_new(i +1) = u(i +1) - u_new(i-1 +1) * factor;
+        u_new(i) = u(i) - u_new(i-1) * factor;
     }
 
-    //v(n-1) = u(n-1) / b_new(n-1);
-    for (int i=n-2; i>=0; i--) {
-        v(i +1) = (u_new(i +1) - v(i+1 +1) * c(i)) / b_new(i);
+    v(n-1) = u(n-1) / b_new(n-1);
+    for (int i=n-2; i>0; i--) {
+        v(i) = (u_new(i) - v(i+1) * c(1)) / b_new(i); //unsure which c,no matter
     }
 
     return v;
