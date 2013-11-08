a823edf4c59f38a2a6fedb5de10a3f1b9e5994ec
PaulMag <paulms@ulrik.uio.no>
2013-11-08 15:28:43 +0100

    Added crankNicolson() primitively by copying parts of the other methods. Everything works, so task (b) is mostly done.

--- algorithms.cpp
+++ algorithms.cpp
@@ -65,7 +65,7 @@ vec backwardEuler(int n, double time, double dt) {
 
     // Initial value:
     vec u     = zeros<vec>(n); // previous timestep
-    double u0 = 1.0;      // this is the element before u(0)
+    double u0 = 1.0; // this is "u(-1)" the element before u(0)
     vec u_alt(n);
     vec u_new(n); // next timestep
 
@@ -100,8 +100,63 @@ vec backwardEuler(int n, double time, double dt) {
     return u;
 }
 
-/*
+
 vec crankNicolson(int n, double time, double dt) {
 
+    double alpha = 0.5 * dt * (n+1) * (n+1); // this is alpha/2
+
+    vec a(n);
+    vec b(n);
+    vec b_alt(n);
+    vec c(n);
+
+    // Fill in arrays:
+    for (int i=0; i<n; i++) {
+        a(i) = -alpha;
+        b(i) = 1 + 2 * alpha;
+        c(i) = -alpha;
+    }
+
+    // Initial value:
+    vec u     = zeros<vec>(n); // previous timestep
+    double u0 = 1.0; // this is "u(-1)" the element before u(0)
+    vec u_alt(n);
+    vec u_mid(n); // half timestep
+    vec u_new(n); // next timestep
+
+
+    // Time loop:
+    for (double j=0; j*dt < time; j++) {
+
+        // Forward euler algorithm:
+        u_mid(0) = u(0) + ( u(1) - 2 * u(0) + u0 ) * alpha; // first element
+        for (int i=1; i<n-1; i++) {
+            u_mid(i) = u(i) + ( u(i+1) - 2 * u(i) + u(i-1) ) * alpha;
+        }
+
+        // Backward euler algorithm:
+        b_alt(0) = b(0);
+        u_alt(0) = u_mid(0) - u0 * a(0);
+
+        for (int i=1; i<n; i++) {
+            /* Forwards substitution. */
+            double factor = a(i) / b_alt(i-1); // avoids doing this twice
+
+            b_alt(i) = b(i)     - factor * c(i-1);
+            u_alt(i) = u_mid(i) - factor * u_alt(i-1);
+        }
+
+        u_new(n-1) = u_mid(n-1) / b_alt(n-1);
+        for (int i=n-2; i>=0; i--) {
+            /* Backwards substitution. */
+            u_new(i) = (u_alt(i) - u_new(i+1) * c(i)) / b_alt(i);
+        }
+
+        // Update:
+        for (int i=0; i<n; i++) {
+            u(i) = u_new(i);
+        }
+    }
+
+    return u;
 }
-*/
